c3e3f5b2e334918b38c72174599a26ec0e5093f3
Chris <chrishethertonweb@gmail.com>
2022-11-24 16:53:25 +0100

    took info, need to know and conditional rendering from menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,15 +7,15 @@ function NavBar() {
         <Fragment>
             <div className="navBar">
                 <Link to='/'>HOME</Link>
-                <Link to='/info'>    INFO</Link>
-                <Link to='/needToKnow'>    NEED TO KNOW</Link>
+                {/* <Link to='/info'>    INFO</Link> */}
+                {/* <Link to='/needToKnow'>    NEED TO KNOW</Link> */}
                 <Link to='/basics'>     REACT BASICS</Link>
                 <Link to='/components'>    COMPONENTS</Link>
                 <Link to='/state'>    State</Link>
-                <Link to='/conditionalRendering'>    CONDITIONAL RENDERING</Link>
+                {/* <Link to='/conditionalRendering'>    CONDITIONAL RENDERING</Link> */}
                 <Link to='/eventListeners'>    EVENT LISTENERS</Link>
                 <Link to='/routerSection' >    ROUTER SECTION</Link>
-                <Link to='/contextApi' >    CONTEXT API</Link>
+                <Link to='/contextApi' >    CONTEXT</Link>
                 <Link to='/useReducer'>    USE REDUCER</Link>
             </div>
             <Outlet/>
