b5ded18017428b35d44b1d80931bce50fef53fd8
Chris <chrishethertonweb@gmail.com>
2022-11-24 15:00:55 +0100

    refactored OutletSection

--- src/sub-components/router-sub-components/OutletSection.js
+++ src/sub-components/router-sub-components/OutletSection.js
@@ -3,18 +3,16 @@ import Outlet2 from '../../images/Outlet2.png'
 import Outlet3 from '../../images/Outlet3.png'
 import Outlet4 from '../../images/Outlet4.png'
 
-
-
-
 function OutletSection() {
     return ( 
         <section className="outlet">
             <h3>What is <code>Outlet</code> ?</h3>
             <p>It is nothing but a place holder that allows child Routes components to be rendered. Not like <code>children</code>, it is used only for this scope.</p>
-            <p>Don't forget to import from <code>react-router-dom</code>.</p>
             <div>
                 <img src={Outlet1} alt="Outlet1" />
             </div>
+            <p>Don't forget to import from <code>react-router-dom</code>.</p>
+
             <div>
                 <img src={Outlet2} alt="Outlet2" />
             </div>
