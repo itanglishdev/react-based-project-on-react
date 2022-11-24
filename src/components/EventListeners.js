cddaab198329111caa4249bfc8833f2c471fe5dc
Kairos22 <alandiko84@gmail.com>
2022-11-24 18:51:54 +0100

    Add state and events

--- src/components/EventListeners.js
+++ src/components/EventListeners.js
@@ -1,9 +1,33 @@
+import eventImage from "../images/react-event.png";
+
 function EventListeners() {
-    return ( 
-        <div className="event-listener">
-            This is the Event Listener section
-        </div>
-     );
+  return (
+    <div className="event-listener">
+      <section className="react-event">
+        <h2 className="reactEvent_h2">React Events</h2>
+        <p className="reactEvent_p">
+          Just like <em>HTML DOM</em> events, <em>React</em> can perform actions
+          based on user events. <em>React</em> has the same events as HTML:{" "}
+          <em>click</em>, <em>change</em>, <em>mouseover</em> etc.
+        </p>
+      </section>
+      <section className="addingEvents">
+        <h3 className="addingevents_h3">Adding Events</h3>
+        <p className="addingevents_p">
+          React events are written in camelCase syntax: onClick instead of
+          onclick. React event handlers are written inside curly braces:
+          onClick={`{`}shoot{`}`} instead of onClick="shoot()".
+        </p>
+        <section className="reactEvent_section">
+          <h4 className="reactevent_h4">React:</h4>
+
+          <div className="react-event_img">
+            <img src={eventImage} alt="" />
+          </div>
+        </section>
+      </section>
+    </div>
+  );
 }
 
 export default EventListeners;
--- src/components/State.js
+++ src/components/State.js
@@ -1,35 +1,63 @@
 import useEffectImage1 from "../images/useEffect1.png";
+import useEffectImage2 from "../images/useEffect2.png";
+import useEffectImage3 from "../images/useEffect3.png";
+import useStateImage1 from "../images/useState.png";
+import importState from "../images/importstate.png";
+import updateState from "../images/updateState.png";
 
 function State() {
   return (
-    <section className="hooks">
-      <h4>What is a Hook?</h4>
-      <p>
-        A Hook is a special function that lets you “hook into” React features.
-        For example, <em>useState</em> is a Hook that lets you add React state
-        to function components.{" "}
-      </p>
-      <h1 className="use_state_h1">useState</h1>
-      <div>
+    <div className="hooks">
+      <section className="hook">
+        <h4>What is a Hook?</h4>
         <p>
-          React components will often need dynamic information in order to
-          render. For example, imagine a component that displays the score of a
-          basketball game. The score of the game might change over time, meaning
-          that the score is dynamic. Our component will have to know the score,
-          a piece of dynamic information, in order to render in a useful way.
+          A Hook is a special function that lets you “hook into” React features.
+          For example, <em>useState</em> is a Hook that lets you add React state
+          to function components.{" "}
         </p>
-        <p>
-          There are two ways for a component to get dynamic information:{" "}
-          <em>props</em> and <em>state</em>.
-        </p>
-        <h2 className="use_state_h2">
-          Import <em>useState</em>
-        </h2>
-        <p className="use_state_p">
-          When we use <em>useState</em> we need to import it into our component.
-        </p>
-      </div>
-      <div className="use-effect">
+      </section>
+      <section className="useState">
+        <h1 className="use_state_h1">useState</h1>
+        <div>
+          <p>
+            React components will often need dynamic information in order to
+            render. For example, imagine a component that displays the score of
+            a basketball game. The score of the game might change over time,
+            meaning that the score is dynamic. Our component will have to know
+            the score, a piece of dynamic information, in order to render in a
+            useful way.
+          </p>
+          <p>
+            There are two ways for a component to get dynamic information:{" "}
+            <em>props</em> and <em>state</em>.
+          </p>
+          <div className="state_img">
+            <img src={useStateImage1} alt="" />
+          </div>
+          <section className="importState">
+            <h3 className="use_state_h2">
+              Import <em>useState</em>
+            </h3>
+            <p className="use_state_p">
+              When we use <em>useState</em> we need to import it into our
+              component.
+            </p>
+            <div className="importState_img">
+              <img src={importState} alt="" />
+            </div>
+          </section>
+          <section className="updatestate">
+            <h3 className="updatestate_h3">Update State</h3>
+            <p className="updatestate_p">
+              To update our state, we use our state updater function.
+            </p>
+            <div className="updateState_img">
+              <img src={updateState} alt="" />
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="use-effect">
         <h2 className="use_effect_h2">useEffect</h2>
         <p>
           The <em>Effect Hook</em> lets you perform side effects in function
@@ -39,8 +67,20 @@ function State() {
         <div className="effect_img">
           <img src={useEffectImage1} alt="" />
         </div>
-      </div>
-    </section>
+        <div>
+          <h4>Runs on every render</h4>
+          <div className="effect_img">
+            <img src={useEffectImage2} alt="" />
+          </div>
+        </div>
+        <div>
+          <h4>Props or state values</h4>
+          <div className="effect_img">
+            <img src={useEffectImage3} alt="" />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
